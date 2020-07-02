0755d8c53a68053474ca3430a875652e25303e5d
Xiang Wang <ramwin@qq.com>
2020-07-02 12:27:53 +0800

    drop django1

--- stored_messages/models.py
+++ stored_messages/models.py
@@ -3,13 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
-from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 from .settings import stored_messages_settings
 
 
-@python_2_unicode_compatible
 class Message(models.Model):
     """
     This model represents a message on the database. Fields are the same as in
@@ -25,20 +23,18 @@ class Message(models.Model):
         return self.message
 
 
-@python_2_unicode_compatible
 class MessageArchive(models.Model):
     """
     This model holds all the messages users received. Corresponding
     database table will grow indefinitely depending on messages traffic.
     """
-    user = models.ForeignKey(settings.AUTH_USER_MODEL)
-    message = models.ForeignKey(Message)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
 
     def __str__(self):
         return "[%s] %s" % (self.user, self.message)
 
 
-@python_2_unicode_compatible
 class Inbox(models.Model):
     """
     Inbox messages are stored in this model until users read them. Once read,
@@ -46,8 +42,8 @@ class Inbox(models.Model):
     that they could be removed by a proper django command. We do not expect
     database table corresponding to this model to grow much.
     """
-    user = models.ForeignKey(settings.AUTH_USER_MODEL)
-    message = models.ForeignKey(Message)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name_plural = _('inboxes')
